56e9a0cbe9ec8c86097a9bf3ca83228443259045
Arghajit Singha <arghajitsingha@MacBookAir.lan>
2024-12-27 16:15:35 +0530

    Adding monocart report

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,28 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "blob" : "html",
+  reporter: process.env.CI
+    ? [
+        ["list"],
+        ["blob"],
+        [
+          "monocart-reporter",
+          {
+            name: "My Test Report",
+            outputFile: "./monocart-report/index.html",
+          },
+        ],
+      ]
+    : [
+        ["html"],
+        [
+          "monocart-reporter",
+          {
+            name: "My Test Report",
+            outputFile: "./monocart-report/index.html",
+          },
+        ],
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
