80f2b1dded58ed660394cb74bb30ee3bc52e2fd9
Arghajit Singha <arghajitsingha@MacBookAir.lan>
2025-01-04 15:39:29 +0530

    visualTesting-01 Added visual testing

---
+++ utils/helper-function.page.js
@@ -0,0 +1,53 @@
+const { expect } = require("@playwright/test");
+import resemble from "resemblejs";
+import fs from "fs";
+
+export class HelperFunction {
+  constructor(page) {
+    this.page = page;
+  }
+
+  async compareScreenshots(currentPath, baselinePath, diffPath) {
+    return new Promise((resolve, reject) => {
+      resemble(baselinePath)
+        .compareTo(currentPath)
+        // .ignoreAntialiasing()
+        .onComplete((data) => {
+          try {
+            if (data && data.getBuffer) {
+              fs.writeFileSync(diffPath, data.getBuffer(true));
+            }
+            resolve(data.rawMisMatchPercentage);
+          } catch (error) {
+            reject({ error, screenshotPath: diffPath });
+          }
+        });
+    });
+  }
+
+  async wait() {
+    await this.page.waitForLoadState("domcontentloaded");
+    await this.page.waitForTimeout(4000);
+  }
+
+  async captureBase64Screenshot(diffPath) {
+    return diffPath.toString("base64");
+  }
+
+  async validateMismatch(mismatch, diffPath) {
+    try {
+      expect(parseFloat(mismatch)).toBeLessThan(1);
+    } catch (error) {
+      const screenshotBase64 = await this.captureBase64Screenshot(diffPath);
+
+      // Log the error message with the base64 encoded screenshot
+      const errorMessage = `Mismatch for Home page: ${mismatch}, Error Screenshot path: ${screenshotBase64}`;
+
+      // Log the error with the HTML image tag
+      console.error(errorMessage);
+
+      // Throw a custom error with the HTML content and base64 screenshot
+      throw new Error(errorMessage);
+    }
+  }
+}
